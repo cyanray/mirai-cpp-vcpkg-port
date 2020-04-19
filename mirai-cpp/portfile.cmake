5121f1778ba2617e3a7ea4e27e0d476248dbd467
Cyan <a484599279@hotmail.com>
2020-04-19 15:11:27 +0800

    mirai-cpp v0.3.0-alpha

--- mirai-cpp/portfile.cmake
+++ mirai-cpp/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/cyanray/mirai-cpp/archive/v0.2.0-alpha.zip"
-    FILENAME "mirai-cpp 0.2.0.zip"
-    SHA512 9644565486f8e710f98f4772331b69a40bb2875425c0614f10082468691cfa9e48597a6b954ef3aa78ab408b2a112932e30ff41d39a9cb9a5ea425165e15755d
+    URLS "https://github.com/cyanray/mirai-cpp/archive/v0.3.0-alpha.zip"
+    FILENAME "mirai-cpp 0.3.0.zip"
+    SHA512 2591dc50dc856b7f104b2f4ce4c712e5f3f69a84a2597aa5914e312bf95db385f61bb314e645c9a131a3f46ba5189911fea5cf08828a3231dded9f4bb784f67a
 )
 
 vcpkg_extract_source_archive_ex(
